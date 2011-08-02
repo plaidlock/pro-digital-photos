c2e7b0fae7aac72d39939775c130767c33ac0d79
Seth Vargo <sethvargo@gmail.com>
2011-08-02 14:59:23 -0400

    fixing compass to work on heroku

--- config/compass.rb
+++ config/compass.rb
@@ -6,17 +6,10 @@ project_type = :rails
 http_path = "/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+output_style = :compressed #:expanded or :nested or :compact or :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+css_dir      = 'tmp/stylesheets'
+sass_dir     = 'app/views/stylesheets'
