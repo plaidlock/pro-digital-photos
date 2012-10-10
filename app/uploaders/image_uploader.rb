2bbcc7666ffc6465f4ee164360feca2733172166
Seth Vargo <sethvargo@gmail.com>
2012-10-10 10:44:57 -0400

    fixing carrier wave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Use file storage
-  storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ default_run_options[:pty] = true
 
 # Defaults
 set :application, 'pro-digital-photos'
-set :deploy_to, "~/www/#{application}"
+set :deploy_to, "/home1/prodigi6/www/#{application}"
 set :use_sudo, false
 
 # Git
@@ -16,24 +16,37 @@ set :scm, 'git'
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
 
-set :user, 'prodigit'
+set :user, 'prodigi6'
 
 # Set RAILS_ENV to production
 set :rails_env, 'production'
 
 # Configs
-role :web, '50.22.11.10'
-role :app, '50.22.11.10'
-role :db,  '50.22.11.10', :primary => true
+role :web, '69.195.124.63'
+role :app, '69.195.124.63'
+role :db,  '69.195.124.63', :primary => true
 
 # Clean up old deploys
 after "deploy", "deploy:cleanup"
 
 # Create .htaccess
 after 'deploy:create_symlink', 'pdphotos:drop_htaccess'
+after 'deploy:create_symlink', 'pdphotos:database_yml'
+
+# pdphotos
 namespace 'pdphotos' do
   task :drop_htaccess, :roles => :app do
     htaccess = "#{current_path}/public/.htaccess"
-    run "if [ ! -f #{htaccess} ]; then echo 'PassengerEnabled On' > #{htaccess}; echo 'PassengerAppRoot #{current_path}' >> #{htaccess}; echo '.htaccess created'; else echo '.htaccess already exists (untouched)'; fi"
+    run "
+      echo 'Options -MultiViews' > #{htaccess}; \
+      echo 'PassengerAppRoot #{current_path}' >> #{htaccess}; \
+      echo 'PassengerResolveSymlinksInDocumentRoot on' >> #{htaccess}; \
+      echo 'RailsEnv production' >> #{htaccess}; \
+      echo 'RackBaseURI /' >> #{htaccess}
+    "
+  end
+
+  task :database_yml, :roles => :app do
+    run "cp ~/secret/database.yml #{current_path}/config/database.yml"
   end
 end
